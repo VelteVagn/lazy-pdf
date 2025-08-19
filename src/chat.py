b85a13e7c589b39c54c4c4661ebd0224794750ac
Vetle <vetle@tjora.net>
2025-08-19 11:30:28 +0200

    refactor: refactor with black

--- src/chat.py
+++ src/chat.py
@@ -7,7 +7,7 @@
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
- 
+
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -22,69 +22,73 @@ from azure.ai.projects import AIProjectClient
 from azure.identity import DefaultAzureCredential
 from azure.ai.agents.models import ListSortOrder, FileSearchTool, FilePurpose
 
-# get the pdf by argument or prompt
-try:
-    file = argv[1]
-    print(f"Welcome! Ask any question about {file} to begin")
-except IndexError:
-    print("Welcome! What pdf should I answer questions about?")
-    file = input()
-    print(f"Ask any question about {file} to begin")
+if __name__ == "__main__":
+    # get the pdf by argument or prompt
+    try:
+        file = argv[1]
+        print(f"Welcome! Ask any question about {file} to begin")
+    except IndexError:
+        print("Welcome! What pdf should I answer questions about?")
+        file = input()
+        print(f"Ask any question about {file} to begin")
 
-# get user input
-user_input = input()
-
-# connect to Azure AI project
-project = AIProjectClient(
-    endpoint="https://vetle-4221-resource.services.ai.azure.com/api/projects/vetle-4221",
-    credential=DefaultAzureCredential(),
-)
-
-# create vector store
-file = project.agents.files.upload(file_path=file, purpose=FilePurpose.AGENTS)
-vector_store = project.agents.vector_stores.create_and_poll(file_ids=[file.id], name="my_file")
-
-# create file search toool and agent
-file_search = FileSearchTool(vector_store_ids=[vector_store.id])
-agent = project.agents.create_agent(
-    model="gpt-4.1-nano",
-    name="gpt-4.1-nano",
-    instructions="You are a helpful assistant answering questions regarding the PDF.",
-    tools=file_search.definitions,
-    tool_resources=file_search.resources,
-)
-
-# start user question/AI answer loop until user types 'exit' or similar
-while user_input.lower() not in ('quit', 'exit', 'cancel'):
-
-    # NOTE: we create a new thread inside the loop to minimise token usage. 
-    #       This will of course make the chat unable to answer follow-ups.
-
-    # create thread and process user input
-    thread = project.agents.threads.create()
-    project.agents.messages.create(
-        thread_id=thread.id,
-        role="user",
-        content=user_input,
-    )
-    run = project.agents.runs.create_and_process(thread_id=thread.id, agent_id=agent.id)
-
-    # handle run errors
-    if run.status=="failed":
-        print(f"run failed: {run.last_error}")
-    
-    # print thread messages
-    messages = project.agents.messages.list(thread_id=thread.id, order=ListSortOrder.ASCENDING)
-    for message in messages:
-        if message.run_id == run.id and message.text_messages:
-            print(f"{message.role}: {message.text_messages[-1].text.value}")
-
-    # get new user imput
+    # get user input
     user_input = input()
 
-# cleanup
-print("exiting...")
-project.agents.vector_stores.delete(vector_store.id)
-project.agents.files.delete(file_id=file.id)
-project.agents.delete_agent(agent.id)
+    # connect to Azure AI project
+    project = AIProjectClient(
+        endpoint="https://vetle-4221-resource.services.ai.azure.com/api/projects/vetle-4221",
+        credential=DefaultAzureCredential(),
+    )
 
+    # create vector store
+    file = project.agents.files.upload(file_path=file, purpose=FilePurpose.AGENTS)
+    vector_store = project.agents.vector_stores.create_and_poll(
+        file_ids=[file.id], name="my_file"
+    )
+
+    # create file search toool and agent
+    file_search = FileSearchTool(vector_store_ids=[vector_store.id])
+    agent = project.agents.create_agent(
+        model="gpt-4.1-nano",
+        name="gpt-4.1-nano",
+        instructions="You are a helpful assistant answering questions regarding the PDF.",
+        tools=file_search.definitions,
+        tool_resources=file_search.resources,
+    )
+
+    # start user question/AI answer loop until user types 'exit' or similar
+    while user_input.lower() not in ("quit", "exit", "cancel"):
+
+        # NOTE: we create a new thread inside the loop to minimise token usage.
+        #       This will of course make the chat unable to answer follow-ups.
+
+        # create thread and process user input
+        thread = project.agents.threads.create()
+        project.agents.messages.create(
+            thread_id=thread.id,
+            role="user",
+            content=user_input,
+        )
+        run = project.agents.runs.create_and_process(thread_id=thread.id, agent_id=agent.id)
+
+        # handle run errors
+        if run.status == "failed":
+            print(f"run failed: {run.last_error}")
+
+        # print thread messages
+        messages = project.agents.messages.list(
+            thread_id=thread.id, order=ListSortOrder.ASCENDING
+        )
+        for message in messages:
+            if message.run_id == run.id and message.text_messages:
+                print(f"{message.role}: {message.text_messages[-1].text.value}")
+
+        # get new user imput
+        user_input = input()
+
+    # cleanup
+    print("exiting...")
+    project.agents.vector_stores.delete(vector_store.id)
+    project.agents.files.delete(file_id=file.id)
+    project.agents.delete_agent(agent.id)
